3f67e5cff6186794d8175aa747601b42741b4cf8
Muhammad Fahad <muhammad.fahad@speridian.com>
2024-01-09 18:51:58 +0500

    new field added

--- Customer.sql
+++ Customer.sql
@@ -4,5 +4,6 @@
     [Name] NCHAR(10) NULL, 
     [Age] INT NULL, 
     [Gender] NCHAR(10) NULL, 
-    [Location] NVARCHAR(50) NULL
+    [Location] NVARCHAR(50) NULL, 
+    [nasb-new-col] NCHAR(10) NULL
 )
